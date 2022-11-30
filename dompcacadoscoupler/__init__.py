4978096bc6a22fe10342f6ad6a83cd7bd020c169
David Zanger <david.zanger@dlr.de>
2022-11-30 17:09:41 +0100

    Adapt init.

--- dompcacadoscoupler/__init__.py
+++ dompcacadoscoupler/__init__.py
@@ -2,3 +2,20 @@
 # By adding imports in this file you can more easily import the classes and functions from this package into other Python modules.
 # Example:
 # from .your_module import your_class
+from sys import platform
+from warnings import warn
+
+import colorama
+from termcolor import colored
+
+colorama.init()
+if platform == "linux" or platform == "linux2":
+    pass
+elif platform == "darwin":
+    # OS X
+    warn(colored(f'Acados may not work on an OS X platform.', 'yellow'),
+         stacklevel=2)
+elif platform == "win32":
+    # Windows
+    warn(colored(f'Acados may not work on a Windows platform.', 'yellow'),
+         stacklevel=2)
