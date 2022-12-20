c2d062db7a67127cba81fa15f4209d726eded5df
David Zanger <david.zanger@dlr.de>
2022-12-20 08:49:36 +0100

    Add tvp and algebraic equations.

--- dompcacadoscoupler/model_converter.py
+++ dompcacadoscoupler/model_converter.py
@@ -20,11 +20,18 @@ def convert_to_acados_model(model: DompcModel,
     acados_model = AcadosModel()
     acados_model.x = cd.vertcat(model.x)
     acados_model.u = cd.vertcat(model.u)
-    acados_model.p = cd.vertcat(model.p)
-    acados_model.f_expl_expr = cd.vertcat(model._rhs)
+    acados_model.p = cd.vertcat(model.p, model.tvp)
+    x_dot_rhs = cd.vertcat(model._rhs)
     x_dot = create_x_dot(model.x)
     acados_model.xdot = x_dot
-    acados_model.f_impl_expr = x_dot - acados_model.f_expl_expr
+    acados_model.f_impl_expr = x_dot - x_dot_rhs
+    algebraic_equations = cd.vertcat(model._alg)
+    acados_model.f_impl_expr = cd.vertcat(acados_model.f_impl_expr,
+                                          algebraic_equations)
     acados_model.z = cd.vertcat(model.z)
+    if algebraic_equations.is_empty() and acados_model.z.is_empty():
+        # The explicit expression can only be provided for a ode not for a dae.
+        # If you have a dae you can only use the implicit solver.
+        acados_model.f_expl_expr = x_dot_rhs
     acados_model.name = name
     return acados_model
