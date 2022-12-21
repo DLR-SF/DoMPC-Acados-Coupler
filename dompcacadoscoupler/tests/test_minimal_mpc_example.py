53d09caeddc2d72ccdbc8a585539f8d3c9b09a37
David Zanger <david.zanger@dlr.de>
2022-12-21 09:04:36 +0100

    Add new test.

--- dompcacadoscoupler/tests/test_minimal_mpc_example.py
+++ dompcacadoscoupler/tests/test_minimal_mpc_example.py
@@ -1,7 +1,6 @@
 import casadi as cd
 import numpy as np
 from acados_template import AcadosModel, AcadosOcp, AcadosOcpSolver
-from casadi import SX, Function, cos, sin, vertcat
 
 
 def export_simple_model() -> AcadosModel:
@@ -35,6 +34,45 @@ def export_simple_model() -> AcadosModel:
     return model
 
 
+def test_with_algebraic_variable_in_cost_function() -> None:
+    # create ocp object to formulate the OCP
+    ocp = AcadosOcp()
+
+    # set model
+    model = export_simple_model()
+    ocp.model = model
+
+    Tf = 1.0
+    N = 20
+
+    # set dimensions
+    ocp.dims.N = N
+
+    # the 'EXTERNAL' cost type can be used to define general cost terms
+    # NOTE: This leads to additional (exact) hessian contributions when using GAUSS_NEWTON hessian.
+    ocp.cost.cost_type = 'EXTERNAL'
+    ocp.cost.cost_type_e = 'EXTERNAL'
+    ocp.model.cost_expr_ext_cost = model.x**2 + model.u**2 + model.z**2
+    ocp.model.cost_expr_ext_cost_e = model.x**2
+
+    # set constraints
+    ocp.constraints.x0 = np.array([0.0])
+
+    # set options
+    ocp.solver_options.qp_solver = 'PARTIAL_CONDENSING_HPIPM'
+    ocp.solver_options.hessian_approx = 'EXACT'  # 'GAUSS_NEWTON', 'EXACT'
+    ocp.solver_options.integrator_type = 'IRK'
+    ocp.solver_options.nlp_solver_type = 'SQP'  # SQP_RTI, SQP
+
+    # set prediction horizon
+    ocp.solver_options.tf = Tf
+
+    p_init = np.array([2])
+    ocp.parameter_values = p_init
+    ocp_solver = AcadosOcpSolver(ocp, json_file='acados_ocp.json')
+    ocp_solver.solve()
+
+
 def test_minimal_mpc_example() -> None:
     # create ocp object to formulate the OCP
     ocp = AcadosOcp()
@@ -44,8 +82,6 @@ def test_minimal_mpc_example() -> None:
     ocp.model = model
 
     Tf = 1.0
-    nx = model.x.size()[0]
-    nu = model.u.size()[0]
     N = 20
 
     # set dimensions
@@ -63,7 +99,7 @@ def test_minimal_mpc_example() -> None:
 
     # set options
     ocp.solver_options.qp_solver = 'PARTIAL_CONDENSING_HPIPM'
-    ocp.solver_options.hessian_approx = 'GAUSS_NEWTON'  # 'GAUSS_NEWTON', 'EXACT'
+    ocp.solver_options.hessian_approx = 'EXACT'  # 'GAUSS_NEWTON', 'EXACT'
     ocp.solver_options.integrator_type = 'IRK'
     ocp.solver_options.nlp_solver_type = 'SQP'  # SQP_RTI, SQP
 
@@ -76,9 +112,8 @@ def test_minimal_mpc_example() -> None:
     p_now = np.array([3])
     for stage in range(0, ocp_solver.acados_ocp.dims.N):
         ocp_solver.set(stage, 'p', p_now)
-    status = ocp_solver.solve()
-    ocp_solver.print_statistics()
+    ocp_solver.solve()
 
 
 if __name__ == '__main__':
-    test_minimal_mpc_example()
+    test_with_algebraic_variable_in_cost_function()
