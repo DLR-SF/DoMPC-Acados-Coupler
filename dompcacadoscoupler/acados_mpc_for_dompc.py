9da0f36d5ba840853c351e0d04dbc3bee87175f2
David Zanger <david.zanger@dlr.de>
2023-01-05 15:56:28 +0100

    Modify determine solver options.

--- dompcacadoscoupler/acados_mpc_for_dompc.py
+++ dompcacadoscoupler/acados_mpc_for_dompc.py
@@ -366,9 +366,11 @@ def determine_solver_options(mpc: MPC) -> AcadosOcpOptions:
     solver_options.nlp_solver_type = options.get('nlp_solver_type', "SQP")
     solver_options.hessian_approx = options.get('hessian_approx', 'EXACT')
     solver_options.integrator_type = options.get('integrator_type', "IRK")
-    solver_options.sim_method_num_stages = options.get('sim_method_num_stages',
-                                                       4)
-    solver_options.sim_method_num_steps = options.get('sim_method_num_steps', 3)
+    for option_name, value in options.items():
+        setattr(solver_options, option_name, value)
+    # In the context of numerical integration, a stage refers to a single evaluation of the derivative of the system being simulated at a particular time. A step is a single iteration of the integration algorithm and typically consists of multiple stages. For example, the often used Runge-Kutta 4 method has four stages per step.
+    # solver_options.sim_method_num_stages = 4
+    # solver_options.sim_method_num_steps = 1
     # solver_options.qp_solver_tol_stat = 1e-2
     # solver_options.qp_solver_tol_eq = 1e-2
     # solver_options.qp_solver_tol_ineq = 1e-2
