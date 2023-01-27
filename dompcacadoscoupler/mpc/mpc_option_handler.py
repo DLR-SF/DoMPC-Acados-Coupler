b1da7ad222ed1eee316991311722ebf91eaa08ee
David Zanger <david.zanger@dlr.de>
2023-01-27 11:28:31 +0100

    Add set prediction horizon.

--- dompcacadoscoupler/mpc/mpc_option_handler.py
+++ dompcacadoscoupler/mpc/mpc_option_handler.py
@@ -9,13 +9,17 @@ def determine_solver_options(mpc: MPC) -> AcadosOcpOptions:
         options = mpc.acados_options
     else:
         options = {}
-    solver_options.tf = mpc.t_step
+    # Tf is the prediction horizon.
+    solver_options.tf = mpc.t_step * mpc.n_horizon  # type: ignore
     solver_options.qp_solver = options.get('qp_solver',
                                            'FULL_CONDENSING_QPOASES')
     solver_options.nlp_solver_type = options.get('nlp_solver_type', "SQP")
     solver_options.hessian_approx = options.get('hessian_approx', 'EXACT')
     solver_options.integrator_type = options.get('integrator_type', "IRK")
     for option_name, value in options.items():
+        if option_name == 'cost_type':
+            # The cost type was set in the cost object and is not set in the solver options
+            continue
         setattr(solver_options, option_name, value)
     # In the context of numerical integration, a stage refers to a single evaluation of the derivative of the system being simulated at a particular time. A step is a single iteration of the integration algorithm and typically consists of multiple stages. For example, the often used Runge-Kutta 4 method has four stages per step.
     # solver_options.sim_method_num_stages = 4
